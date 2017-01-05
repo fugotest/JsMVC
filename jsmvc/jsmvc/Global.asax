725e486d238856941af2bd83a36c11556a9e34f3
patrick <futotest2015@gmail.com>
2017-01-05 17:28:58 +0800

    test

---
+++ jsmvc/jsmvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="jsmvc.MvcApplication" Language="C#" %>
---
+++ jsmvc/jsmvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jsmvc.Startup))]
+namespace jsmvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
